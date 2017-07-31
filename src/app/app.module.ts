91a1d450ac3368ac385724bd2dc267a1327112a5
qingfengwuyou <h3658686@126.com>
2017-07-31 17:07:07 +0800

    no message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
@@ -14,6 +14,7 @@ import { ProductDetailComponent } from './product-detail/product-detail.componen
 import { HomeComponent } from './home/home.component';
 import { Routes, RouterModule } from '@angular/router';
 import { ProductService } from './shared/product.service';
+import { FilterPipe } from './pipe/filter.pipe';
 
 const routes: Routes = [
   {
@@ -41,13 +42,15 @@ const routes: Routes = [
     FooterComponent,
     StarsComponent,
     ProductDetailComponent,
-    HomeComponent
+    HomeComponent,
+    FilterPipe
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    ReactiveFormsModule
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Product, ProductService } from '../shared/product.service';
+import { FormControl } from '@angular/forms';
+import 'rxjs/Rx';
+
 
 @Component({
   selector: 'app-product',
@@ -9,11 +12,19 @@ import { Product, ProductService } from '../shared/product.service';
 export class ProductComponent implements OnInit {
 
   products: Product[];
+  keyword: string;
+
+  searchCtrl: FormControl = new FormControl();
 
   constructor(private productService: ProductService) { }
 
   ngOnInit() {
     this.products = this.productService.getProducts();
+
+    this.searchCtrl.valueChanges
+      .debounceTime(500)
+      .subscribe(value => this.keyword = value);
+
   }
 
 }
